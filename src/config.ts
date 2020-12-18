37f48d7b22e0c73b88d3f4c6c9b33f55f9bab892
predrags <predrags@nvidia.com>
2020-12-18 12:36:59 +0100

    initial

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+'use strict';
+
+export default class Config {
+	public static init(base: string, folder: string) {
+
+	}
+
+	private constructor(base: string, folder: string, print: boolean = false) {
+	}
+}
